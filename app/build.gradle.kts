ce27c29997d2c0b299889841cc4ffbbcbd0bc82f
Alex Clement <alexrdclement@gmail.com>
2023-10-15 12:11:12 -0400

    Bump deps + target SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.alexrdclement.uiplayground"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.alexrdclement.uiplayground"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
