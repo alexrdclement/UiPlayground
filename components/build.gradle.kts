ddd261e80ca2c09037192cd59270bfd95fd8ca3e
Alex Clement <alexrdclement@gmail.com>
2024-01-07 10:33:34 -0500

    Add simple MediaControlBar and MediaControlSheet screenshot tests

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.paparazzi)
 }
 
 android {
@@ -45,6 +46,8 @@ dependencies {
     implementation(libs.material.icons.extended)
 
     testImplementation(libs.junit)
+    testImplementation(libs.test.parameter.injector)
+    testImplementation(projects.testing)
 
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
