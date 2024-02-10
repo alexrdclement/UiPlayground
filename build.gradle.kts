45fcffd16aec26ddef792f7ecb45838bffc06ac4
Alex Clement <alexrdclement@gmail.com>
2024-02-10 09:50:20 -0500

    Bump deps (#14)
    * Bump deps
    
    * Add paparazzi workaround

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,23 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
+
+subprojects {
+    plugins.withId("app.cash.paparazzi") {
+        // Defer until afterEvaluate so that testImplementation is created by Android plugin.
+        afterEvaluate {
+            dependencies.constraints {
+                add("testImplementation", "com.google.guava:guava") {
+                    attributes {
+                        attribute(
+                            TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
+                            objects.named(TargetJvmEnvironment::class.java, TargetJvmEnvironment.STANDARD_JVM)
+                        )
+                    }
+                    because("LayoutLib and sdk-common depend on Guava's -jre published variant." +
+                            "See https://github.com/cashapp/paparazzi/issues/906.")
+                }
+            }
+        }
+    }
+}
