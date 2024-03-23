065dd48bb724cff500976ea27c354e7cab5faeed
Alex Clement <alexrdclement@gmail.com>
2024-03-23 11:48:55 -0400

    Add composition tracing. Set androidx.benchmark.perfettoSdkTracing.enable to true in the run configuration to see in benchmark results.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.foundation)
+    implementation(libs.compose.runtime.tracing)
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.graphics)
     implementation(libs.compose.ui.tooling.preview)
