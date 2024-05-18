dfeba7687cb865ec7cae11ff05c9e6b4173fd8d2
Alex Clement <alexrdclement@gmail.com>
2024-05-18 13:14:17 -0400

    Cleanup

--- shaders/baseline-profile/build.gradle.kts
+++ shaders/baseline-profile/build.gradle.kts
@@ -27,8 +27,6 @@ android {
     targetProjectPath = ":app"
 }
 
-// This is the configuration block for the Baseline Profile plugin.
-// You can specify to run the generators on a managed devices or connected devices.
 baselineProfile {
     useConnectedDevices = true
 }
