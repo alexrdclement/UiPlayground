3913c00dc2f9ccd76b4d2426135017d363969202
Alex Clement <alexrdclement@gmail.com>
2024-08-30 18:06:49 -0400

    Centered amount entry field

--- app/src/main/kotlin/com/alexrdclement/uiplayground/demo/experiments/demo/BasicTextFieldDemo.kt
+++ app/src/main/kotlin/com/alexrdclement/uiplayground/demo/experiments/demo/BasicTextFieldDemo.kt
@@ -2,34 +2,63 @@ package com.alexrdclement.uiplayground.demo.experiments.demo
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation
 import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.byValue
+import androidx.compose.foundation.text.input.insert
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alexrdclement.uiplayground.ui.preview.UiPlaygroundPreview
+import java.text.DecimalFormatSymbols
 
 @Composable
-fun BasicTextFieldDemo() {
-    val textFieldState = rememberTextFieldState()
+fun BasicTextFieldDemo(
+    textFieldState: TextFieldState = rememberTextFieldState(),
+    prefix: String = "$",
+    placeholder: String = "0",
+    groupingSeparator: String = DecimalFormatSymbols.getInstance().groupingSeparator.toString(),
+) {
+    val text by snapshotFlow { textFieldState.text.toString() }
+        .collectAsState(initial = textFieldState.text.toString())
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .systemBarsPadding(),
-        contentAlignment = Alignment.Center,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        BasicText(
+            text = text,
+            style = MaterialTheme.typography.headlineLarge.copy(
+                color = MaterialTheme.colorScheme.onSurface,
+            ),
+        )
+
         BasicTextField(
             state = textFieldState,
             textStyle = MaterialTheme.typography.headlineLarge.copy(
@@ -39,14 +68,51 @@ fun BasicTextFieldDemo() {
                 .width(IntrinsicSize.Min)
                 .background(MaterialTheme.colorScheme.surfaceVariant)
                 .padding(8.dp),
+            inputTransformation = InputTransformation.byValue { _, proposed ->
+                proposed.filter { it.isDigit() }
+            },
+            outputTransformation = {
+                for (index in 1 until originalText.length) {
+                    if (index % 3 == 0) {
+                        insert(originalText.length - index, groupingSeparator)
+                    }
+                }
+            },
             lineLimits = TextFieldLineLimits.SingleLine,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+            ),
             decorator = { textField ->
-                Box(
+                Row(
                     modifier = Modifier
                         .border(2.dp, Color.Red),
-                    contentAlignment = Alignment.Center,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
                 ) {
-                    textField()
+                    BasicText(
+                        prefix,
+                        style = MaterialTheme.typography.headlineLarge.copy(
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        )
+                    )
+
+                    Box {
+                        if (textFieldState.text.isEmpty()) {
+                            // Placeholder
+                            BasicText(
+                                placeholder,
+                                style = MaterialTheme.typography.headlineLarge.copy(
+                                    color = MaterialTheme.colorScheme.onSurfaceVariant.copy(
+                                        alpha = 0.5f,
+                                    ),
+                                )
+                            )
+                        }
+
+                        // Min width to ensure cursor still appears
+                        Box(modifier = Modifier.widthIn(min = 8.dp)) {
+                            textField()
+                        }
+                    }
                 }
             }
         )
