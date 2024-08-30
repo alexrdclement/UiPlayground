e9d2c3941efa2b24a3a8145edbabc55d77d77b2a
Alex Clement <alexrdclement@gmail.com>
2024-08-30 16:47:03 -0400

    Simple centered text field

--- app/src/main/kotlin/com/alexrdclement/uiplayground/demo/experiments/demo/BasicTextFieldDemo.kt
+++ app/src/main/kotlin/com/alexrdclement/uiplayground/demo/experiments/demo/BasicTextFieldDemo.kt
@@ -3,23 +3,26 @@ package com.alexrdclement.uiplayground.demo.experiments.demo
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.alexrdclement.uiplayground.ui.preview.UiPlaygroundPreview
 
 @Composable
 fun BasicTextFieldDemo() {
-    var text by remember { mutableStateOf("") }
+    val textFieldState = rememberTextFieldState()
 
     Box(
         modifier = Modifier
@@ -28,20 +31,32 @@ fun BasicTextFieldDemo() {
         contentAlignment = Alignment.Center,
     ) {
         BasicTextField(
-            value = text,
-            onValueChange = { text = it },
+            state = textFieldState,
             textStyle = MaterialTheme.typography.headlineLarge.copy(
-                color = MaterialTheme.colorScheme.onSurface,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
             ),
-        ) { textField ->
-            Box(
-                modifier = Modifier
-                    .border(2.dp, MaterialTheme.colorScheme.surfaceVariant)
-                    .background(Color.Red),
-                contentAlignment = Alignment.Center,
-            ) {
-                textField()
+            modifier = Modifier
+                .width(IntrinsicSize.Min)
+                .background(MaterialTheme.colorScheme.surfaceVariant)
+                .padding(8.dp),
+            lineLimits = TextFieldLineLimits.SingleLine,
+            decorator = { textField ->
+                Box(
+                    modifier = Modifier
+                        .border(2.dp, Color.Red),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    textField()
+                }
             }
-        }
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    UiPlaygroundPreview {
+        BasicTextFieldDemo()
     }
 }
