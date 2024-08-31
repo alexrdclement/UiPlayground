39f324b1f0f36199a2397aa51e18802d26a145cb
Alex Clement <alexrdclement@gmail.com>
2024-08-31 10:33:55 -0400

    More input filtering. Migrate from InputTransformation.byValue since it didn't work properly when filtering out values.

--- app/src/main/kotlin/com/alexrdclement/uiplayground/demo/experiments/demo/BasicTextFieldDemo.kt
+++ app/src/main/kotlin/com/alexrdclement/uiplayground/demo/experiments/demo/BasicTextFieldDemo.kt
@@ -15,9 +15,9 @@ import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.InputTransformation
+import androidx.compose.foundation.text.input.TextFieldBuffer
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
-import androidx.compose.foundation.text.input.byValue
 import androidx.compose.foundation.text.input.insert
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
@@ -70,7 +70,7 @@ private fun CurrencyAmountField(
     textFieldState: TextFieldState = rememberTextFieldState(),
     placeholder: String = "0",
     includeCurrencyPrefix: Boolean = true,
-    maxNumDecimals: Int = 2
+    maxNumDecimalValues: Int = 2
 ) {
     BasicTextField(
         state = textFieldState,
@@ -82,18 +82,9 @@ private fun CurrencyAmountField(
             .padding(8.dp)
             .background(MaterialTheme.colorScheme.surfaceVariant)
             .padding(8.dp),
-        inputTransformation = InputTransformation.byValue { current, proposed ->
-            val parts = proposed.split(decimalSeparatorStr, limit = 2)
-            val intPart = parts.firstOrNull()?.filter { it.isDigit() }
-            val decimalPart = parts.getOrNull(1)?.filter { it.isDigit() }
-
-            if (decimalPart == null) {
-                intPart ?: ""
-            } else {
-                intPart + decimalSeparatorStr + decimalPart.take(maxNumDecimals)
-            }
-        },
+        inputTransformation = CurrencyAmountFieldInputTransformation(maxNumDecimalValues),
         outputTransformation = {
+            // Insert grouping separators every 3 digits
             val intPart = originalText.split(decimalSeparatorStr, limit = 2).firstOrNull() ?: ""
             for (index in 1 until intPart.length) {
                 if (index % 3 == 0) {
@@ -102,9 +93,6 @@ private fun CurrencyAmountField(
             }
         },
         lineLimits = TextFieldLineLimits.SingleLine,
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Decimal,
-        ),
         decorator = { textField ->
             Row(
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
@@ -138,6 +126,59 @@ private fun CurrencyAmountField(
             }
         }
     )
+}
+
+private class CurrencyAmountFieldInputTransformation(
+    private val maxNumDecimalValues: Int,
+) : InputTransformation {
+    override val keyboardOptions = KeyboardOptions(
+        keyboardType = KeyboardType.Decimal,
+    )
+
+    override fun TextFieldBuffer.transformInput() {
+        val proposed = asCharSequence()
+
+        if (proposed.any { !it.isDigit() && it != decimalSeparator }) {
+            // Reject changes for any non-digit, non-decimal characters
+            revertAllChanges()
+            return
+        }
+
+        if (proposed.count { it == decimalSeparator } > 1) {
+            // Reject changes for multiple decimal separators
+            revertAllChanges()
+            return
+        }
+
+        val parts = proposed.split(decimalSeparatorStr, limit = 2)
+        var intPart = parts.firstOrNull()?.filter { it.isDigit() } ?: ""
+        val decimalPart = parts.getOrNull(1)?.filter { it.isDigit() }
+
+        if (intPart.startsWith('0')) {
+            // Allow single leading zero. Replace leading zero if followed by another digit.
+            val indexOfFirstNonZero = intPart.indexOfFirst { it != '0' }
+            val newIntPart = if (indexOfFirstNonZero > 0) {
+                intPart.substring(indexOfFirstNonZero)
+            } else {
+                "0"
+            }
+            replace(0, intPart.length, newIntPart)
+            intPart = newIntPart
+        }
+
+        if (intPart.isEmpty() && decimalPart != null) {
+            // Prefill 0 when only decimal part is entered
+            val newIntPart = "0"
+            replace(0, intPart.length, newIntPart)
+            intPart = newIntPart
+        }
+
+        if (decimalPart != null && decimalPart.length > maxNumDecimalValues) {
+            // Truncate decimal places
+            val truncatedDecimalPart = decimalPart.take(maxNumDecimalValues)
+            replace(intPart.length + 1, length, truncatedDecimalPart)
+        }
+    }
 }
 
 @Preview
