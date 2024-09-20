1ba11248192f1bca53fb4a5663fd793d7d6372ae
Alex Clement <alexrdclement@gmail.com>
2024-09-20 13:36:19 -0400

    Add BasicTextField wrapper, update DemoTextField

--- app/src/main/kotlin/com/alexrdclement/uiplayground/demo/subject/DemoTextField.kt
+++ app/src/main/kotlin/com/alexrdclement/uiplayground/demo/subject/DemoTextField.kt
@@ -1,16 +1,14 @@
 package com.alexrdclement.uiplayground.demo.subject
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.TextField
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import com.alexrdclement.uiplayground.components.TextField
 import com.alexrdclement.uiplayground.theme.PlaygroundTheme
 
 @Composable
@@ -18,13 +16,13 @@ fun DemoTextField(
     modifier: Modifier = Modifier,
     textStyle: TextStyle = PlaygroundTheme.typography.display,
 ) {
-    var text by remember { mutableStateOf("") }
     Box(modifier = modifier.fillMaxSize()) {
         TextField(
-            value = text,
-            onValueChange = { text = it },
+            state = rememberTextFieldState(),
             textStyle = textStyle,
-            modifier = modifier.align(Alignment.Center)
+            modifier = modifier
+                .background(PlaygroundTheme.colorScheme.background)
+                .align(Alignment.Center)
         )
     }
 }
---
+++ components/src/main/kotlin/com/alexrdclement/uiplayground/components/TextField.kt
@@ -0,0 +1,78 @@
+package com.alexrdclement.uiplayground.components
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation
+import androidx.compose.foundation.text.input.KeyboardActionHandler
+import androidx.compose.foundation.text.input.OutputTransformation
+import androidx.compose.foundation.text.input.TextFieldDecorator
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import com.alexrdclement.uiplayground.theme.PlaygroundTheme
+
+@Composable
+fun TextField(
+    state: TextFieldState,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    inputTransformation: InputTransformation? = null,
+    textStyle: TextStyle = LocalTextStyle.current,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    onKeyboardAction: KeyboardActionHandler? = null,
+    lineLimits: TextFieldLineLimits = TextFieldLineLimits.Default,
+    onTextLayout: (Density.(getResult: () -> TextLayoutResult?) -> Unit)? = null,
+    interactionSource: MutableInteractionSource? = null,
+    cursorBrush: Brush = TextFieldDefaults.CursorBrush,
+    outputTransformation: OutputTransformation? = null,
+    decorator: TextFieldDecorator? = null,
+    scrollState: ScrollState = rememberScrollState(),
+) {
+    BasicTextField(
+        state = state,
+        modifier = modifier,
+        enabled = enabled,
+        readOnly = readOnly,
+        inputTransformation = inputTransformation,
+        textStyle = textStyle,
+        keyboardOptions = keyboardOptions,
+        onKeyboardAction = onKeyboardAction,
+        lineLimits = lineLimits,
+        onTextLayout = onTextLayout,
+        interactionSource = interactionSource,
+        cursorBrush = cursorBrush,
+        outputTransformation = outputTransformation,
+        decorator = decorator,
+        scrollState = scrollState,
+    )
+}
+
+object TextFieldDefaults {
+    val CursorBrush: Brush
+        @Composable
+        get() = SolidColor(PlaygroundTheme.colorScheme.primary)
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    PlaygroundTheme {
+        Surface {
+            TextField(
+                state = rememberTextFieldState("text"),
+            )
+        }
+    }
+}
