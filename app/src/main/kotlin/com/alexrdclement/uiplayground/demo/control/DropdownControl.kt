cbaa292f844e903dc113a17a43c130c785a7d547
Alex Clement <alexrdclement@gmail.com>
2024-09-29 08:47:54 -0400

    Add DropdownMenu, DropdownMenuItem. Heavily copied from material3.

--- app/src/main/kotlin/com/alexrdclement/uiplayground/demo/control/DropdownControl.kt
+++ app/src/main/kotlin/com/alexrdclement/uiplayground/demo/control/DropdownControl.kt
@@ -3,8 +3,6 @@ package com.alexrdclement.uiplayground.demo.control
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -14,8 +12,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.BlurredEdgeTreatment
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.alexrdclement.uiplayground.components.Button
+import com.alexrdclement.uiplayground.components.DropdownMenu
+import com.alexrdclement.uiplayground.components.DropdownMenuItem
 import com.alexrdclement.uiplayground.components.Text
 import com.alexrdclement.uiplayground.theme.PlaygroundTheme
 import com.alexrdclement.uiplayground.ui.preview.UiPlaygroundPreview
