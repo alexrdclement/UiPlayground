c64637e7c30affad9b0fc773a143797a7db4a673
Alex Clement <alexrdclement@gmail.com>
2024-10-20 09:42:23 -0400

    Add iOS project with build phase set up

---
+++ iosApp/UiPlayground/UiPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiplaygroundApp.swift
+//  uiplayground
+//
+//  Created by Alex Clement on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct UiPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
