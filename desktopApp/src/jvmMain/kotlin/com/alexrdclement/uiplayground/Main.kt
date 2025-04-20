f2833c413de890037b30ebb5ea4fd4637a3d7a63
Alex Clement <alexrdclement@gmail.com>
2025-04-20 14:45:44 -0400

    Center window on launch

--- desktopApp/src/jvmMain/kotlin/com/alexrdclement/uiplayground/Main.kt
+++ desktopApp/src/jvmMain/kotlin/com/alexrdclement/uiplayground/Main.kt
@@ -1,7 +1,9 @@
 package com.alexrdclement.uiplayground
 
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.alexrdclement.uiplayground.app.App
@@ -9,7 +11,11 @@ import com.alexrdclement.uiplayground.app.App
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        state = rememberWindowState(width = 1200.dp, height = 960.dp),
+        state = rememberWindowState(
+            width = 1200.dp,
+            height = 960.dp,
+            position = WindowPosition.Aligned(Alignment.Center),
+        ),
         title = "UiPlayground",
     ) {
         App()
