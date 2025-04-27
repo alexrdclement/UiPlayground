bab6bb2357af4f2317bc34b8b2b46e4a581bd589
Alex Clement <alexrdclement@gmail.com>
2025-04-27 11:09:54 -0400

    Bump to Compose mp 1.8.0-rc01

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -45,6 +45,7 @@ kotlin {
                 api(compose.ui)
                 implementation(compose.foundation)
                 implementation(compose.material3)
+                implementation(compose.materialIconsExtended)
                 implementation(compose.uiTooling)
                 implementation(compose.components.uiToolingPreview)
 
