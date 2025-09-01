2fb5839350fc4d1cbe0abe2b2ad62399781825d9
Alex Clement <alexrdclement@gmail.com>
2025-09-01 18:44:06 -0400

    Fix top padding in demos

--- app/src/commonMain/kotlin/com/alexrdclement/uiplayground/app/demo/components/demo/GridDemo.kt
+++ app/src/commonMain/kotlin/com/alexrdclement/uiplayground/app/demo/components/demo/GridDemo.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
@@ -262,10 +261,9 @@ fun GridDemo(
     )
 
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
             .fillMaxSize()
-            .statusBarsPadding(),
-        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Grid(
             style = gridStyle,
