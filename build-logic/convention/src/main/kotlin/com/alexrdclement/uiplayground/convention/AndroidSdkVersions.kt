323f277004e06fc62bc1f51a3b016c8e999f7265
Alex Clement <alexrdclement@gmail.com>
2025-09-21 12:02:26 -0400

    Reorder again

--- build-logic/convention/src/main/kotlin/com/alexrdclement/uiplayground/convention/AndroidSdkVersions.kt
+++ build-logic/convention/src/main/kotlin/com/alexrdclement/uiplayground/convention/AndroidSdkVersions.kt
@@ -1,5 +1,5 @@
 package com.alexrdclement.uiplayground.convention
 
 const val AndroidMinSdk = 24
-const val AndroidCompileSdk = 36
 const val AndroidTargetSdk = 36
+const val AndroidCompileSdk = 36
