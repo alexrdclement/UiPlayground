5ed919bc2fd1dc883c9d37eda760eeafd14721f2
Alex Clement <alexrdclement@gmail.com>
2025-12-31 09:35:41 -0500

    Delete uievent and trace

--- trace/src/commonMain/kotlin/com/alexrdclement/uiplayground/trace/ReportDrawn.kt
+++
@@ -1,6 +0,0 @@
-package com.alexrdclement.uiplayground.trace
-
-import androidx.compose.runtime.Composable
-
-@Composable
-expect fun ReportDrawn()
--- trace/src/commonMain/kotlin/com/alexrdclement/uiplayground/trace/Trace.kt
+++
@@ -1,3 +0,0 @@
-package com.alexrdclement.uiplayground.trace
-
-expect inline fun <T> trace(label: String, block: () -> T): T
